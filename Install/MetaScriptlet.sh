136f7ccede6bfe55decf352f55bb48cba89dc69d
minecraftylogan <114712942+minecraftylogan@users.noreply.github.com>
2025-07-19 17:28:52 -0500

    Update MetaScriptlet.sh
    okay guys, I think we got it

--- Install/MetaScriptlet.sh
+++ Install/MetaScriptlet.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 echo 'Scriptlet-MetaData'
-#okie now we are going to check for the file existence
-#KindleFetch
-if [ -f /mnt/us/extensions/KindleFetch/run.sh ] 
-    then 
-        curl https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/KindleFetch.sh -O -L /mnt/us/Documents/
-        fi
-#Alpine
-if [ -f /mnt/us/alpine_kindle_kual/start/start_gui.sh ]
-    then
-        curl https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/Alpine.sh -O -L /mnt/us/Documents/
-        fi
-#KoReader
-if [ -f /mnt/us/koreader/koreader.sh ]
-    then
-        curl https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/KOReader.sh -O -L /mnt/us/Documents/
-        fi
-#Meta-Scriptlet
-curl https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/Meta-Script.sh -O -L /mnt/us/Documents/
 
+# KindleFetch
+if [ -f /mnt/us/extensions/KindleFetch/run.sh ]; then
+    curl -L -o /mnt/us/Documents/KindleFetch.sh https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/KindleFetch.sh
+fi
+
+# Alpine
+if [ -f /mnt/us/alpine_kindle_kual/start/start_gui.sh ]; then
+    curl -L -o /mnt/us/Documents/Alpine.sh https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/Alpine.sh
+fi
+
+# KoReader
+if [ -f /mnt/us/koreader/koreader.sh ]; then
+    curl -L -o /mnt/us/Documents/KOReader.sh https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/KOReader.sh
+fi
+
+# Meta-Scriptlet (always downloaded)
+curl -L -o /mnt/us/Documents/Meta-Script.sh https://github.com/minecraftylogan/Scriptlet-MetaData/raw/refs/heads/main/Meta-Script.sh
